80654e155ddf01d2b8598b42ba84f42dafa0f183
KietPham-VN <phamanhkiet.dev@gmail.com>
2026-01-17 01:13:00 +0700

    feat(database): remove unnecessary data annotations from DatabaseOptions

--- Backend/Src/PIED-LMS.Infrastructure/Options/DatabaseOptions.cs
+++ Backend/Src/PIED-LMS.Infrastructure/Options/DatabaseOptions.cs
@@ -1,24 +1,13 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace PIED_LMS.Infrastructure.Options;
 
 public class DatabaseOptions
 {
     public const string SectionName = "Database";
 
-    [Required]
     public string Host { get; init; } = string.Empty;
-
-    [Range(1, 65535)]
     public int Port { get; init; } = 5432;
-
-    [Required]
     public string Name { get; init; } = string.Empty;
-
-    [Required]
     public string User { get; init; } = string.Empty;
-
-    [Required]
     public string Password { get; init; } = string.Empty;
 
     public string ConnectionString =>
